2821985a04b70280dc570f776190c37e7e70022b
Mauricio <mxr210005@utdallas.edu>
2025-01-21 14:07:57 -0600

    login page, no backend yet

---
+++ musicboxdfrontend/src/components/InputField.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+interface Props{
+    type: string;
+    placeholder: string;
+    icon: string;
+}
+const InputField = ({ type, placeholder, icon }: Props) => {
+  // State to toggle password visibility
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+  return (
+    <div className="input-wrapper">
+      <input
+        type={isPasswordShown ? 'text' : type}
+        placeholder={placeholder}
+        className="input-field"
+        required
+      />
+      <i className="material-symbols-outlined">{icon}</i>
+      {type === 'password' && (
+        <i onClick={() => setIsPasswordShown(prevState => !prevState)} className="material-symbols-outlined eye-icon">
+          {isPasswordShown ? 'visibility' : 'visibility_off'}
+        </i>
+      )}
+    </div>
+  )
+}
+export default InputField;
---
+++ musicboxdfrontend/src/components/SocialLogin.tsx
@@ -0,0 +1,11 @@
+const SocialLogin = () => {
+    return (
+      <div className="social-login">
+        <button className="social-button">
+          <img src="spotify.svg" alt="Spotify" className="social-icon" />
+          Spotify
+        </button>
+      </div>
+    )
+  }
+  export default SocialLogin;
