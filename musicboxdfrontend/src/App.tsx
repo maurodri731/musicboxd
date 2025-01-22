8e3b5d80b3692a77182ae874ca790bcb5f627a1f
Mauricio <mxr210005@utdallas.edu>
2025-01-21 21:07:38 -0600

    added navbar and bootstrap

--- musicboxdfrontend/src/App.tsx
+++ musicboxdfrontend/src/App.tsx
@@ -1,21 +1,12 @@
-import SocialLogin from "./components/SocialLogin";
-import InputField from "./components/InputField";
+import Login from "./components/Login";
+import NavbarComp from "./components/NavbarComp";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const App = () => {
   return (
-    <div className="login-container">
-      <h2 className="form-title">Log in with</h2>
-      <SocialLogin />
-      <p className="separator"><span>or</span></p>
-      <form action="#" className="login-form">
-        <InputField type="email" placeholder="Email address" icon="mail" />
-        <InputField type="password" placeholder="Password" icon="lock" />
-        <a href="#" className="forgot-password-link">Forgot password?</a>
-        <button type="submit" className="login-button">Log In</button>
-      </form>
-      <p className="signup-prompt">
-        Don't have an account? <a href="#" className="signup-link">Sign up</a>
-      </p>
-    </div>
+      <div>
+        <Login/>
+      </div>
   )
 }
 export default App;
---
+++ musicboxdfrontend/src/components/Login.tsx
@@ -0,0 +1,23 @@
+import SocialLogin from "./SocialLogin";
+import InputField from "./InputField";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+    <h2 className="form-title">Log in with</h2>
+    <SocialLogin />
+    <p className="separator"><span>or</span></p>
+    <form action="#" className="login-form">
+      <InputField type="email" placeholder="Email address" icon="mail" />
+      <InputField type="password" placeholder="Password" icon="lock" />
+      <a href="#" className="forgot-password-link">Forgot password?</a>
+      <button type="submit" className="login-button">Log In</button>
+    </form>
+    <p className="signup-prompt">
+      Don't have an account? <a href="#" className="signup-link">Sign up</a>
+    </p>
+  </div>
+  )
+}
+
+export default Login
